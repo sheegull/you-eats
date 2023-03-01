8ce93ab5c0caddfdcdf83a6fd6408b11dd89561f
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-01 17:43:07 +0900

    update

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -8,6 +8,7 @@ import { GiDiamondTrophy, GiEclipse } from "react-icons/gi";
 import { RiLightbulbLine, RiFeedbackLine } from "react-icons/ri";
 import { FiSettings, FiHelpCircle } from "react-icons/fi";
 import { HiOutlineShoppingBag } from "react-icons/hi";
+
 export const categories = [
   { name: "New", icon: <AiFillHome />, type: "home" },
   { name: "New", icon: <AiFillHome />, type: "home" },
