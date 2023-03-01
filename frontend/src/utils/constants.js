70069bb2b85cb8569c077208f1cab56a20d2c6cd
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-01 17:42:52 +0900

    ✨ create constants

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,19 @@
+import { AiFillHome, AiOutlineFlag } from "react-icons/ai";
+import { MdLocalFireDepartment, MdLiveTv } from "react-icons/md";
+import { CgMusicNote } from "react-icons/cg";
+import { FiFilm } from "react-icons/fi";
+import { IoGameControllerSharp } from "react-icons/io5";
+import { ImNewspaper } from "react-icons/im";
+import { GiDiamondTrophy, GiEclipse } from "react-icons/gi";
+import { RiLightbulbLine, RiFeedbackLine } from "react-icons/ri";
+import { FiSettings, FiHelpCircle } from "react-icons/fi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+export const categories = [
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+  { name: "New", icon: <AiFillHome />, type: "home" },
+];
